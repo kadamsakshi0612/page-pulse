99c40e4e3a8eabb639e5f0526aca4efc172d44bd
Sakshi Kadam <kadamsakshi938@gmail.com>
2026-07-24 20:33:27 +0530

    Fix Render Deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 import time
+import os
 
 app = Flask(__name__)
 
@@ -81,4 +82,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
